5c681538878109bde67658cedc90a3781bdffc6c
kuckjwi <rnrghks09@gmail.com>
2024-04-02 21:31:16 +0900

    initial commit

---
+++ src/main/java/io/kuckjwi/example/CallWorkflowJavaExampleApplication.java
@@ -0,0 +1,11 @@
+package io.kuckjwi.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallWorkflowJavaExampleApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CallWorkflowJavaExampleApplication.class, args);
+  }
+}
---
+++ src/test/java/io/kuckjwi/example/CallWorkflowJavaExampleApplicationTests.java
@@ -0,0 +1,12 @@
+package io.kuckjwi.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallWorkflowJavaExampleApplicationTests {
+  @Test
+  void contextLoads() {
+    System.out.println("Context Loaded!");
+  }
+}
